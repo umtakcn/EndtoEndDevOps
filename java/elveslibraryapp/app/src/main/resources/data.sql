1794ff949267555aa5ed2aa186a2b25fa0b2085b
Umut Akcan <60771816+umtakcn@users.noreply.github.com>
2021-06-04 10:50:20 +0300

    Update data.sql

--- java/elveslibraryapp/app/src/main/resources/data.sql
+++ java/elveslibraryapp/app/src/main/resources/data.sql
@@ -1,14 +1,14 @@
 INSERT INTO elves (id, name) VALUES
-  (1, 'Noldor'),
-  (2, 'Teleri'),
-  (3, 'Vanyar');
+  (1, 'Vanyar'),
+  (2, 'Noldor'),
+  (3, 'Teleri');
 
-INSERT INTO definition (id, source, value) VALUES
-  (1, 'https://www.definitions.net/definition/Noldor', 'In the works of J. R. R. Tolkien, the Noldor (also spelled Ñoldor, meaning those with knowledge in Quenya) are High Elves of the Second Clan who migrated to Valinor and lived in Eldamar'),
-  (2, 'https://www.definitions.net/definition/Teleri', 'In the works of J. R. R. Tolkien, the Teleri, Those who come last in Quenya (singular Teler) were the third of the Elf clans who came to Aman'),
-  (3, 'https://www.glyphweb.com/arda/t/teleri.php', 'The third and greatest of the three Elf-kindreds of the Great Journey, from whom the Nandor and the Sindar were descended. The Teleri came last to Aman, and dwelt in Alqualondë on the western shores of the Great Sea.');
+INSERT INTO definition (id, value) VALUES
+  (1, 'The Vanyar, also called the Fair Elves, Light-elves, and Spear-elves, were the first and smallest of the Kindreds of the Eldar.'),
+  (2, 'The Ñoldor or in Third Age Middle-earth meaning those with knowledge, were the second clan of the Elves, alson known as ME'),
+  (3, 'The Teleri, meaning Those who come last, were the third of the Elf clans who came to Aman. Those who came to Aman became known as the Falmari.');
 
 INSERT INTO elves_definitions (elves_id, definitions_id) VALUES
   (1, 1),
   (2, 2),
-  (2, 3);
+  (3, 3);
